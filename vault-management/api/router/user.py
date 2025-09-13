906f0c4a115b23db880e03c623bf918680546a31
iamPMV259 <minhvu2592005@gmail.com>
2025-09-14 00:40:34 +0700

    add get user balance api(#7)

--- vault-management/api/router/user.py
+++ vault-management/api/router/user.py
@@ -28,3 +28,27 @@ async def create_user(user_wallet: str):
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create user: {str(e)}")
+
+
+@router.get("/balance", response_model=float)
+async def get_user_balance(user_wallet: str, vault_name: str):
+    r"""Get the balance of a user in a specific vault.
+
+    Inputs:
+        user_wallet (str): The wallet address of the user.
+        vault_name (str): The name of the vault.
+
+    Outputs:
+        On success, returns the balance of the user in the specified vault.
+        On failure, raises an HTTPException with status 404 and an error message
+        describing the failure.
+    """
+    try:
+        balance = await UserOperations.get_user_balance(user_wallet, vault_name)
+        return balance
+    except ResourceNotFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to get user balance: {str(e)}"
+        )
--- vault-management/backend/user.py
+++ vault-management/backend/user.py
@@ -1,6 +1,6 @@
 from configs import get_logger
 from hooks.error import ResourceNotFound
-from mongo.schemas import UserMetadata
+from mongo.schemas import UserBalanceHistory, UserMetadata, VaultsMetadata
 from utils import hasher
 
 logger = get_logger("user_operations")
@@ -21,3 +21,20 @@ class UserOperations:
             _ = await new_user.save()
             logger.info(f"User with wallet {wallet_address} created successfully.")
             return new_user
+
+    @staticmethod
+    async def get_user_balance(user_wallet: str, vault_name: str) -> float:
+        user = await UserMetadata.find_one(UserMetadata.wallet_address == user_wallet)
+        if not user:
+            raise ResourceNotFound(f"User with wallet {user_wallet} not found.")
+        vault = await VaultsMetadata.find_one(VaultsMetadata.name == vault_name)
+        if not vault:
+            raise ResourceNotFound(f"Vault with name {vault_name} not found.")
+        user_balance = await UserBalanceHistory.find_one(
+            And(
+                UserBalanceHistory.user == user.id,
+                UserBalanceHistory.vault == vault.id,
+            ),
+            sort=[("update_at", -1)],
+        )
+        return user_balance.balance if user_balance else 0.0
