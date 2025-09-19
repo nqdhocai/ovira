d5d795beb8d4f5785aa86110c3086b93a7a46375
an-npt17 <truongan1412@gmail.com>
2025-09-19 15:35:16 +0700

    update base url

--- strategy_engine/agents/model.py
+++ strategy_engine/agents/model.py
@@ -9,10 +9,12 @@ def get_llm_model():
     global _llm_model
     if _llm_model is None:
         _llm_model = init_chat_model(
-            model_provider=agents_config.LLM_MODEL_PROVIDER,
             model=agents_config.LLM_MODEL_NAME,
+            model_provider=agents_config.LLM_MODEL_PROVIDER,
             api_key=agents_config.LLM_MODEL_API_KEY,
             temperature=agents_config.LLM_MODEL_TEMPERATURE,
             max_tokens=agents_config.LLM_MODEL_MAX_TOKENS,
+            api_version=agents_config.LLM_MODEL_API_VERSION,
+            base_url=agents_config.LLM_MODEL_BASE_URL,
         )
     return _llm_model
